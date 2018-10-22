067b4c714338a4a0bba340d339db9708157d47a4
Elise Hinman <43010657+edhinman@users.noreply.github.com>
2018-10-22 14:32:39 -0600

    update to dataPrep()
    working on total vs. dissolved check....

--- R/dataPrep.r
+++ R/dataPrep.r
@@ -26,19 +26,53 @@ translation_wb="P:\\WQ\\Integrated Report\\Automation_Development\\elise\\demo\\
 unit_sheetname="unitConvTable"
 startRow=1
 
+######################################################
+###Data prep for dissolved vs. total fraction check###
+######################################################
 
-#Load translation workbook
+# Reduce dataframe to columns of interest for comparing total and dissolved, narrow down rows to unique values (currently values are duplicated if measurement subject to multiple uses/standards)
+unique(data$ResultSampleFractionText)# note that NA's may be in this list.
+data1 <- data[,names(data)%in%c("ActivityStartDate","ActivityIdentifier","R3172ParameterName","FractionGroup","IR_Unit", "IR_Value")]
+data1 <- unique(data1)
+
+# Separate into TOTAL and DISSOLVED objects, and give unit/value columns unique names specific to total or dissolved
+tot <- subset(data1, data1$FractionGroup=="TOTAL")
+dim(tot)
+tot <- tot[,!names(tot)%in%c("FractionGroup")]
+names(tot)[names(tot)=="IR_Value"]<- "IR_Value_Tot"
+names(tot)[names(tot)=="IR_Unit"]<- "IR_Unit_Tot"
+
+diss <- subset(data1, data1$FractionGroup=="DISSOLVED")
+dim(diss)
+diss <- diss[,!names(diss)%in%c("FractionGroup")]
+names(diss)[names(diss)=="IR_Value"]<- "IR_Value_Diss"
+names(diss)[names(diss)=="IR_Unit"]<- "IR_Unit_Diss"
+
+# Merge TOTAL and DISSOLVED objects based on AID, Start Date, and Parameter name
+diss_tot <- merge(tot,diss, by=c("ActivityIdentifier","ActivityStartDate","R3172ParameterName"))
+
+# Find all unique combos of TOTAL and DISSOLVED units to run through updateUnitConvTable function.
+unit_combos <- diss_tot[,names(diss_tot)%in%c("IR_Unit_Tot","IR_Unit_Diss")]
+# NOTE: IR_Unit_Diss arbitrarily made TARGET (CriterionUnits) unit.
+colnames(unit_combos) <- c("IR_Unit","CriterionUnits") # make column names consistent with unitConvTable
+
+updateUnitConvTable(unit_combos, translation_wb, sheetname="unitConvTable", startRow=1, startCol=1)
+
+# Bind unit_combo columns to diss_tot columns (maintain original column names differentiating total with dissolved, but include duplicated columns with unitConvTable column names)
+diss_tot <- cbind(diss_tot,unit_combos)
+
+##################################
+###Unit conversion table checks###
+##################################
+
+#Load translation workbook updated from comparison of TOTAL/DISSOLVED units above.
 trans_wb=openxlsx::loadWorkbook(translation_wb)
 
 #Remove filters from all sheets in trans_wb (filtering seems to cause file corruption occassionally...)
 sheetnames=openxlsx::getSheetNames(translation_wb)
 for(n in 1:length(sheetnames)){
-	openxlsx::removeFilter(trans_wb, sheetnames[n])
-	}
-
-######################
-###Unit conversions###
-######################
+  openxlsx::removeFilter(trans_wb, sheetnames[n])
+}
 
 #Read unit conversion table 
 unit_convs=data.frame(openxlsx::readWorkbook(trans_wb, sheet=unit_sheetname, startRow=startRow, detectDates=TRUE))
@@ -51,6 +85,21 @@ names(unit_convs)[names(unit_convs)=="IR_FLAG"]="IR_UnitConv_FLAG"
 if(any(is.na(unit_convs$IR_UnitConv_FLAG))){
   print("WARNING: Unit conversion table missing required IR_FLAG information. Please correct the table before proceeding.")
 }
+
+# Double check that blanks are all NA in data (shouldn't really need this at this point)
+diss_tot[diss_tot==""]=NA
+
+# Merge conversion table to data (up through this point, same functionality as applyScreenTable)
+diss_tot=merge(diss_tot,unit_convs,all.x=T)
+dim(diss_tot)
+
+#Perform conversions
+diss_tot$IR_Value_Tot=diss_tot$IR_Value_Tot*diss_tot$UnitConversionFactor
+diss_tot$IR_Unit_Tot=diss_tot$CriterionUnits
+
+##################################
+###Unit conversions for IR data###
+##################################
 
 #Double check that blanks are all NA in data (shouldn't really need this at this point)
 data[data==""]=NA
@@ -76,11 +125,6 @@ data$UnitConversionFactor[data$IR_Unit==data$CriterionUnits]=1
 #Convert IR_Value using Unit Conversion Value
 data$IR_Value <- data$IR_Value*data$UnitConversionFactor
 data$IR_Unit = data$CriterionUnits
-
-########################################
-###Dissolved vs. total fraction check###
-########################################
-
 
 #Aggregate to daily values
 
