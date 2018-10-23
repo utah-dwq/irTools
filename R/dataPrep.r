ecbbaeb1fa0349411668efd2caa5f809e604c765
Elise Hinman <43010657+edhinman@users.noreply.github.com>
2018-10-23 16:07:26 -0600

    edited tot>diss section
    1. created data_prep_FLAG/REASON
    2. screen tot>dis records and merge back to data_crit
    3. screens unit conversions and performs unit conversions.

--- R/dataPrep.r
+++ R/dataPrep.r
@@ -32,34 +32,24 @@ startRow=1
 
 # Reduce dataframe to columns of interest for comparing total and dissolved, narrow down rows to unique values (currently values are duplicated if measurement subject to multiple uses/standards)
 unique(data$ResultSampleFractionText)# note that NA's may be in this list.
-data1 <- data[,names(data)%in%c("ActivityStartDate","ActivityIdentifier","R3172ParameterName","FractionGroup","IR_Unit", "IR_Value")]
+data1 <- data[,names(data)%in%c("ActivityStartDate","ActivityIdentifier","R3172ParameterName","FractionGroup","IR_Unit", "IR_Value","Data_Prep_FLAG","Data_Prep_REASON")]
 data1 <- unique(data1)
 
 # Separate into TOTAL and DISSOLVED objects, and give unit/value columns unique names specific to total or dissolved
+#***NOTE***IR_Unit column in tot retains original "IR_Unit", while IR_Unit in diss converted to "CriterionUnits" (consistent with updateUnitConvTable function)
 tot <- subset(data1, data1$FractionGroup=="TOTAL")
 dim(tot)
-tot <- tot[,!names(tot)%in%c("FractionGroup")]
+tot <- tot[,!names(tot)%in%c("FractionGroup","Data_Prep_FLAG","Data_Prep_REASON")]
 names(tot)[names(tot)=="IR_Value"]<- "IR_Value_Tot"
-names(tot)[names(tot)=="IR_Unit"]<- "IR_Unit_Tot"
 
 diss <- subset(data1, data1$FractionGroup=="DISSOLVED")
 dim(diss)
 diss <- diss[,!names(diss)%in%c("FractionGroup")]
 names(diss)[names(diss)=="IR_Value"]<- "IR_Value_Diss"
-names(diss)[names(diss)=="IR_Unit"]<- "IR_Unit_Diss"
+names(diss)[names(diss)=="IR_Unit"]<- "CriterionUnits"
 
 # Merge TOTAL and DISSOLVED objects based on AID, Start Date, and Parameter name
 diss_tot <- merge(tot,diss, by=c("ActivityIdentifier","ActivityStartDate","R3172ParameterName"))
-
-# Find all unique combos of TOTAL and DISSOLVED units to run through updateUnitConvTable function.
-unit_combos <- diss_tot[,names(diss_tot)%in%c("IR_Unit_Tot","IR_Unit_Diss")]
-#***NOTE***IR_Unit_Diss arbitrarily made TARGET (CriterionUnits) unit.
-names(unit_combos)[names(unit_combos)=="IR_Unit_Diss"] <- "CriterionUnits"
-names(unit_combos)[names(unit_combos)=="IR_Unit_Tot"] <- "IR_Unit"
-updateUnitConvTable(unit_combos, translation_wb, sheetname="unitConvTable", startRow=1, startCol=1)
-
-# Bind unit_combo columns to diss_tot columns (maintain original column names differentiating total with dissolved, but include duplicated columns with unitConvTable column names)
-diss_tot <- cbind(diss_tot,unit_combos)
 
 ##################################
 ###Unit conversion table checks###
@@ -98,20 +88,23 @@ dim(diss_tot_units)
 
 #Convert IR_Unit/Value_Tot to same units as IR_Unit/Value_Diss
 diss_tot_units$IR_Value_Tot=diss_tot_units$IR_Value_Tot*diss_tot_units$UnitConversionFactor
-diss_tot_units$IR_Unit_Tot=diss_tot_units$CriterionUnits
+diss_tot_units$IR_Unit=diss_tot_units$CriterionUnits
 
-# Set all comparisons to baseline ACCEPT
-diss_tot_units$Dis_Tot_FLAG = "ACCEPT"
+# Find comparisons where DISSOLVED > TOTAL and set Data_Prep_FLAG to REJECT
+diss_tot_units$Data_Prep_FLAG = "ACCEPT" # start them all off as ACCEPT
+diss_tot_units$Data_Prep_FLAG <- ifelse(diss_tot_units$IR_Value_Tot<diss_tot_units$IR_Value_Diss,"REJECT",diss_tot_units$Data_Prep_FLAG)
 
-# Find comparisons where TOTAL > DISSOLVED
-diss_tot_units$Dis_Tot_FLAG <- ifelse(diss_tot_units$IR_Value_Tot>diss_tot_units$IR_Value_Diss,"REJECT",diss_tot_units$Dis_Tot_FLAG)
+# Provide Data_Prep_REASON
+diss_tot_units$Data_Prep_REASON = "ACCEPT" # start them all off as ACCEPT
+diss_tot_units$Data_Prep_REASON[(diss_tot_units$IR_Value_Tot<diss_tot_units$IR_Value_Diss) & (diss_tot_units$Data_Prep_REASON!="ACCEPT")]<- paste(diss_tot_units$Data_Prep_REASON,"Dissolved fraction greater than total fraction", sep=",") #concatenate multiple reasons
+diss_tot_units$Data_Prep_REASON[(diss_tot_units$IR_Value_Tot<diss_tot_units$IR_Value_Diss) & (diss_tot_units$Data_Prep_REASON=="ACCEPT")]<- "Dissolved fraction greater than total fraction"
 
 # Merge Dis_Tot_FLAG info back to data.
-ds_test <- diss_tot_units[,names(diss_tot_units)%in%c("ActivityIdentifier","ActivityStartDate","R3172ParameterName","Dis_Tot_FLAG")]
+ds_test <- diss_tot_units[,names(diss_tot_units)%in%c("ActivityIdentifier","ActivityStartDate","R3172ParameterName","Data_Prep_FLAG","Data_Prep_REASON")]
 data <- merge(data,ds_test, all.x=TRUE)
 dim(data)
-unique(data$Dis_Tot_FLAG)
-table(data$Dis_Tot_FLAG)
+unique(data$Data_Prep_FLAG)
+unique(data$Data_Prep_REASON)
 
 ##################################
 ###Unit conversions for IR data###
