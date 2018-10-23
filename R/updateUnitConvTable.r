f88a9cdbf82d3e9e917d4b5b6e4667ae81c7be90
Elise Hinman <43010657+edhinman@users.noreply.github.com>
2018-10-23 12:54:20 -0600

    added tot/dis unit combos to update unit conv table.
    added tot/dis unit combos to update unit conv table.

--- R/updateUnitConvTable.r
+++ R/updateUnitConvTable.r
@@ -18,9 +18,9 @@
 #' @export
 updateUnitConvTable=function(data, translation_wb, sheetname="unitConvTable", startRow=1, startCol=1){
 
-
 ######TESTING SETUP
-# translation_wb="P:\\WQ\\Integrated Report\\Automation_Development\\R_package\\demo\\03translation\\ir_translation_workbook.xlsx"
+# translation_wb="P:\\WQ\\Integrated Report\\Automation_Development\\elise\\demo\\03translation\\ir_translation_workbook.xlsx"
+# #translation_wb="P:\\WQ\\Integrated Report\\Automation_Development\\R_package\\demo\\03translation\\ir_translation_workbook.xlsx"
 # data=data_crit
 # sheetname="unitConvTable"
 # startRow=1
@@ -37,13 +37,41 @@ for(n in 1:length(sheetnames)){
 	openxlsx::removeFilter(trans_wb, sheetnames[n])
 	}
 
+#######Unit Conversions######
+##TOTAL versus DISSOLVED Fraction Units
+# Reduce dataframe to columns of interest for comparing total and dissolved, narrow down rows to unique values (currently values are duplicated if measurement subject to multiple uses/standards)
+unique(data$ResultSampleFractionText)# note that NA's may be in this list.
+data1 <- data[,names(data)%in%c("ActivityStartDate","ActivityIdentifier","R3172ParameterName","FractionGroup","IR_Unit", "IR_Value")]
+data1 <- unique(data1)
 
-####Unit Conversions
+# Separate into TOTAL and DISSOLVED objects, and give unit/value columns unique names specific to total or dissolved
+#NOTE:DISSOLVED IR_Unit arbitrarily made TARGET (CriterionUnits) unit.
+tot <- subset(data1, data1$FractionGroup=="TOTAL")
+dim(tot)
+tot <- tot[,!names(tot)%in%c("FractionGroup")]
+names(tot)[names(tot)=="IR_Value"]<- "IR_Value_Tot"
+
+diss <- subset(data1, data1$FractionGroup=="DISSOLVED")
+dim(diss)
+diss <- diss[,!names(diss)%in%c("FractionGroup")]
+names(diss)[names(diss)=="IR_Value"]<- "IR_Value_Diss"
+names(diss)[names(diss)=="IR_Unit"]<- "CriterionUnits"
+
+# Merge TOTAL and DISSOLVED objects based on AID, Start Date, and Parameter name
+diss_tot <- merge(tot,diss, by=c("ActivityIdentifier","ActivityStartDate","R3172ParameterName"))
+
+# Find all unique combos of TOTAL and DISSOLVED units to run through updateUnitConvTable function.
+dtunits <- data.frame(unique(diss_tot[,c("IR_Unit","CriterionUnits")]))
+
+##IR_Unit versus CriterionUnits
 #Identify unique combinations of IR_Unit and CriterionUnits.
 rcunits=data.frame(unique(data[,c("IR_Unit", "CriterionUnits")]))
-rcunits$InData="Y"
-rcunits[rcunits==""]<-NA
 #rcunits=na.omit(rcunits)
+
+##Combine these unit conversions into one dataframe
+all_unit_combos <- rbind(dtunits,rcunits)
+all_unit_combos$InData="Y"
+all_unit_combos[all_unit_combos==""]<-NA
 
 # Load unitConvTable sheet, set all blanks to NA, and reset InData column.
 unitconv_table=data.frame(openxlsx::readWorkbook(trans_wb, sheet="unitConvTable", startRow=startRow, detectDates=TRUE))
@@ -51,7 +79,7 @@ column_names=names(unitconv_table)
 unitconv_table=unitconv_table[,!names(unitconv_table)%in%"InData"]
 
 # Merge NEW unique combinations of ResultMeasure.MeasureUnitCode/CriterionUnits to unitConvTable.
-unit_merge=merge(rcunits,unitconv_table,all=T)
+unit_merge=merge(all_unit_combos,unitconv_table,all=T)
 unit_merge=unit_merge[,column_names]
 
 # Document date new combination added.
