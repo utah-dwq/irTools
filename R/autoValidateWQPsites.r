1ddd2e311b9eab9ae7b73a58511ca50c85db9bde
jakevl <39382528+jakevl@users.noreply.github.com>
2018-10-30 15:24:41 -0600

    Update R/autoValidateWQPsites.r

--- R/autoValidateWQPsites.r
+++ R/autoValidateWQPsites.r
@@ -37,28 +37,28 @@ autoValidateWQPsites=function(sites_file,master_site_file,polygon_path,outfile_p
 
 ###########
 #####TESTING SETUP
-library(sp)
-library(sf)
-sites_file="P:\\WQ\\Integrated Report\\Automation_Development\\R_package\\demo\\01raw_data\\sites161001-180930.csv"
-#sites_file="P:\\WQ\\Integrated Report\\Automation_Development\\elise\\demo\\01raw_data\\sites101001-180930_EHduptest.csv"
-master_site_file="P:\\WQ\\Integrated Report\\Automation_Development\\R_package\\demo\\02site_validation\\wqp_master_site_file.csv"
-polygon_path="P:\\WQ\\Integrated Report\\Automation_Development\\R_package\\demo\\02site_validation\\polygons"
-outfile_path="P:\\WQ\\Integrated Report\\Automation_Development\\R_package\\demo\\02site_validation"
-correct_longitude=FALSE
-site_type_keep=c("Lake, Reservoir, Impoundment",
-			 "Stream",
-			 "Spring",
-			 "Stream: Canal",
-			 "Stream: Ditch",
-			 "River/Stream",
-			 "Lake",
-			 "River/Stream Intermittent",
-			 "River/Stream Perennial",
-			 "Reservoir",
-			 "Canal Transport",
-			 "Canal Drainage",
-			 "Canal Irrigation")
-########
+#library(sp)
+#library(sf)
+#sites_file="P:\\WQ\\Integrated Report\\Automation_Development\\R_package\\demo\\01raw_data\\sites161001-180930.csv"
+##sites_file="P:\\WQ\\Integrated Report\\Automation_Development\\elise\\demo\\01raw_data\\sites101001-180930_EHduptest.csv"
+#master_site_file="P:\\WQ\\Integrated Report\\Automation_Development\\R_package\\demo\\02site_validation\\wqp_master_site_file.csv"
+#polygon_path="P:\\WQ\\Integrated Report\\Automation_Development\\R_package\\demo\\02site_validation\\polygons"
+#outfile_path="P:\\WQ\\Integrated Report\\Automation_Development\\R_package\\demo\\02site_validation"
+#correct_longitude=FALSE
+#site_type_keep=c("Lake, Reservoir, Impoundment",
+#			 "Stream",
+#			 "Spring",
+#			 "Stream: Canal",
+#			 "Stream: Ditch",
+#			 "River/Stream",
+#			 "Lake",
+#			 "River/Stream Intermittent",
+#			 "River/Stream Perennial",
+#			 "Reservoir",
+#			 "Canal Transport",
+#			 "Canal Drainage",
+#			 "Canal Irrigation")
+#########
 
 
 setwd(outfile_path)
