ca33bf42e6d40b8fa66d237d2898ed884a4b5bbf
jakevl <39382528+jakevl@users.noreply.github.com>
2018-12-05 09:57:41 -0700

    Trying expand_uses

--- R/rollUp.R
+++ R/rollUp.R
@@ -6,19 +6,25 @@
 #' @param group_vars Vector of column names on which to group data for assessment rollups. Defaults to subset by ASSESS_ID, BeneficialUse, and R3172ParameterName.
 #' @return Returns dataframe with assessment categories for each AU/BenUse/R3172ParameterName.
 #' @importFrom plyr rbind.fill
+#' @importFrom reshape2 colsplit
 #' @export
 
 
-rollUp <- function(data, group_vars=c("ASSESS_ID","BeneficialUse","R3172ParameterName")){ #(JV) I generalized function name to just rollUp. I think we may want to perform rollups at both site & AU spatial scales
+rollUp <- function(data, group_vars=c("ASSESS_ID","BeneficialUse","R3172ParameterName"), expand_uses=TRUE){ #(JV) I generalized function name to just rollUp. I think we may want to perform rollups at both site & AU spatial scales
 
 # Testing
- #data=list(toxics_assessed,conv_assessed) # compile these into a list in assessIR script? - (JV) I think pass as a vector of object names, then use mget to convert to list, then do.call w/ rbind.fill. See below.
- data=c("toxics_assessed", "conv_assessed")
- group_vars=c("IR_MLID","R317Descrp","ASSESS_ID","AU_NAME", "BeneficialUse", "R3172ParameterName")
+#data=list(toxics_assessed,conv_assessed) # compile these into a list in assessIR script? - (JV) I think pass as a vector of object names, then use mget to convert to list, then do.call w/ rbind.fill. See below.
+data=c("toxics_assessed", "conv_assessed")
+group_vars=c("ASSESS_ID","AU_NAME", "BeneficialUse")
+expand_uses=TRUE
 
 # Combine all assessed data into one dataframe for roll up
 dat=mget(data)
 dat_all=do.call(plyr::rbind.fill, dat)
+
+
+if(expand_uses & !"BEN_CLASS" %in% group_vars){group_vars=append(group_vars, "BEN_CLASS")}
+
 
 # ****PLACE HOLDER****
 # Read in list of sites with TMDLs, and redefine IR_Cat for those sites as "TMDLa" (TMDL approved)
@@ -57,6 +63,33 @@ rollup=within(rollup,{
 })
 
 
+
+if(expand_uses){
+	#Expand comma separated uses (BEN_CLASS)
+	uses=rollup[,group_vars]
+	uses$BEN_CLASS=as.character(uses$BEN_CLASS)
+	max_use_count=max(sapply(strsplit(uses$BEN_CLASS,","),FUN="length"))
+	use_colnames=paste0(rep("use",max_use_count),seq(1:max_use_count))
+	uses_mat=unique(data.frame(uses$BEN_CLASS,reshape2::colsplit(uses$BEN_CLASS,",",use_colnames)))
+	names(uses_mat)[names(uses_mat)=="uses.BEN_CLASS"]="BEN_CLASS"
+	
+	#Flatten uses
+	uses_flat=reshape2::melt(uses_mat, id.vars="BEN_CLASS", value.name = "BeneficialUse")
+	uses_flat=uses_flat[,!names(uses_flat)=="variable"]
+	uses_flat=uses_flat[uses_flat$BeneficialUse!="" & !is.na(uses_flat$BeneficialUse),]
+	
+	uses=uses[,!names(uses) %in% "BeneficialUse"]
+	
+	uses_expanded=merge(uses,uses_flat, all.x=T, by="BEN_CLASS")
+	
+	rollup=merge(uses_expanded, rollup, all.x=T)
+	rollup$AssessCat[is.na(rollup$AssessCat)]="NA"
+}
+
+
+head(rollup)
+
+
 if("BeneficialUse" %in% group_vars){
 	print("Beneficial use assessment category frequencies:")
 	print(table(rollup$BeneficialUse, rollup$AssessCat))
@@ -67,6 +100,12 @@ if("R3172ParameterName" %in% group_vars){
 	print(table(rollup$R3172ParameterName[rollup$AssessCat=="NS"]))
 }
 
+
+
+
 return(rollup)
 
 }
+
+
+
