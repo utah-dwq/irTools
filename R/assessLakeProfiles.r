528f976c76f823b55e1798ffe0f7c4518de5342a
jakevl <39382528+jakevl@users.noreply.github.com>
2019-01-22 10:57:39 -0700

    delete readECHO, create lake prof func

---
+++ R/assessLakeProfiles.r
@@ -0,0 +1,22 @@
+#' Run lake profile assessments
+#'
+#' Description description description
+#'
+#' @param data 
+#' @return Returns dataframe with assessment categories for each AU/BenUse/R3172ParameterName.
+
+#' @export
+assessLakeProfiles <- function(data){
+
+#### Testing setup
+#data=
+####
+
+
+
+return(profile_asmnts)
+
+}
+
+
+
