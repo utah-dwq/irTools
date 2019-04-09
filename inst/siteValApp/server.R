3db84ea357a44036292c18cedc97ba2701c36919
jakevl <39382528+jakevl@users.noreply.github.com>
2019-04-08 18:02:27 -0600

    accept button v1

--- inst/siteValApp/server.R
+++ inst/siteValApp/server.R
@@ -5,8 +5,8 @@ server <- function(input, output, session){
 #setwd('C:\\Users\\jvander\\Documents\\R\\irTools\\inst\\siteValApp')
 library(wqTools)
 library(leaflet)
-sites=read.csv(file='IR_master_site_file.csv')
-reasons=read.csv(file='rev_rej_reasons.csv')
+sites=read.csv(file='IR_master_site_file.csv', stringsAsFactors=F)
+reasons=read.csv(file='rev_rej_reasons.csv', stringsAsFactors=F)
 sites=within(sites,{
 	lat=ifelse(!is.na(IR_Lat), IR_Lat, LatitudeMeasure)
 	long=ifelse(!is.na(IR_Long), IR_Long, LongitudeMeasure)
@@ -87,9 +87,9 @@ observeEvent(reactive_objects$map_sites, ignoreNULL = F, ignoreInit=T, {
 observeEvent(input$map_marker_click, {
 	site_click <- input$map_marker_click
 	siteid=site_click$id
-	lat=round(as.numeric(paste(unique(reactive_objects$sites[reactive_objects$sites$MonitoringLocationIdentifier == siteid,'lat']))[1]),4)
-	long=round(as.numeric(paste(unique(reactive_objects$sites[reactive_objects$sites$MonitoringLocationIdentifier == siteid,'long']))[1]),4)
-	latlong_matches=reactive_objects$sites[round(reactive_objects$sites$lat,4)==lat & round(reactive_objects$sites$long,4)==long,]
+	lat=round(as.numeric(paste(unique(reactive_objects$map_sites[reactive_objects$map_sites$MonitoringLocationIdentifier == siteid,'lat']))[1]),4)
+	long=round(as.numeric(paste(unique(reactive_objects$map_sites[reactive_objects$map_sites$MonitoringLocationIdentifier == siteid,'long']))[1]),4)
+	latlong_matches=reactive_objects$map_sites[round(reactive_objects$map_sites$lat,4)==lat & round(reactive_objects$map_sites$long,4)==long,]
 	if(dim(latlong_matches)[1] > 1){siteid = as.character(unique(latlong_matches$MonitoringLocationIdentifier))}
 
 	if(!is.null(siteid)){
@@ -107,15 +107,15 @@ observeEvent(input$map_marker_click, {
 observeEvent(reactive_objects$selected_sites, ignoreNULL=F, {
 	map_proxy %>%
 	clearGroup(group='highlight') %>%
-	addCircleMarkers(data=reactive_objects$sites[reactive_objects$sites$MonitoringLocationIdentifier %in% reactive_objects$selected_sites,],
+	addCircleMarkers(data=reactive_objects$map_sites[reactive_objects$map_sites$MonitoringLocationIdentifier %in% reactive_objects$selected_sites,],
 		group='highlight', options = pathOptions(pane = "highlight"), radius = 20, color='chartreuse', opacity = 0.75, fillOpacity = 0.4)
 })
 
 
 # Selected sites table
-output$selected_sites_table=DT::renderDataTable({
+output$selected_sites_table=DT::renderDT({
 	req(reactive_objects$selected_sites)
-	reactive_objects$selected_sites_table=as.data.frame(reactive_objects$sites[reactive_objects$sites$MonitoringLocationIdentifier %in% reactive_objects$selected_sites,
+	reactive_objects$selected_sites_table=as.data.frame(reactive_objects$map_sites[reactive_objects$map_sites$MonitoringLocationIdentifier %in% reactive_objects$selected_sites,
 		c("MonitoringLocationIdentifier","OrganizationIdentifier","MonitoringLocationName","MonitoringLocationTypeName",
 		  "IR_FLAG_REASONS","IR_FLAG", "OrganizationFormalName","ProviderName","IR_MLID","IR_MLNAME","ASSESS_ID",
 		  "AU_NAME","AU_Type","Water_Type","R317Descrp","ss_R317Descrp",
@@ -128,27 +128,78 @@ output$selected_sites_table=DT::renderDataTable({
 })
 
 
-# Reviewer actions
-observeEvent(input$clear_sel, {
-	table_selected=reactive_objects$selected_sites_table[input$selected_sites_table_rows_selected,]$MonitoringLocationIdentifier
-	reactive_objects$selected_sites=reactive_objects$selected_sites[!reactive_objects$selected_sites %in% table_selected]
+# Table selections
+observe({
+	req(reactive_objects$selected_sites_table)
+	reactive_objects$table_selected_table=reactive_objects$selected_sites_table[input$selected_sites_table_rows_selected,]
+	reactive_objects$table_selected_mlids=reactive_objects$table_selected_table$MonitoringLocationIdentifier
+	
 })
 
+# Reviewer actions
+## Clear table selection
+observeEvent(input$clear_sel, {
+	reactive_objects$selected_sites=reactive_objects$selected_sites[!reactive_objects$selected_sites %in% reactive_objects$table_selected_mlids]
+})
+
+## Clear all selected sites
 observeEvent(input$clear_all, {
 	reactive_objects$selected_sites=NULL
 })
 
-#observeEvent(input$accept, {})
+## Accept selected sites
+observeEvent(input$accept, {
+	if(length(reactive_objects$table_selected_mlids)==0){
+		showModal(modalDialog(title="Error.",size="l",easyClose=T,
+			"Select site(s) in map & table to make a review.")
+	)}else{
+		showModal(modalDialog(title="OK to ACCEPT site(s)?",size="l", footer=NULL,
+		DT::renderDT({
+			DT::datatable(reactive_objects$table_selected_table,
+				selection='none', rownames=FALSE, filter="none",
+				options = list(scrollY = TRUE, paging = FALSE, scrollX=TRUE, dom="t")
+			)
+		}),
+		br(),
+		br(),
+		actionButton('accept_ok', 'Accept', style='color: #fff; background-color: #337ab7; border-color: #2e6da4;font-size:120%'),
+		actionButton('accept_cancel', 'Cancel', style='color: #fff; background-color: #337ab7; border-color: #2e6da4;font-size:120%')
+		))
+	}
+})
+
+### Update attributes
+observeEvent(input$accept_cancel, {removeModal()})
+observeEvent(input$accept_ok, {
+	accept_mlids=reactive_objects$table_selected_mlids
+	reactive_objects$sites=within(reactive_objects$sites, {
+		IR_FLAG[MonitoringLocationIdentifier %in% accept_mlids] = "ACCEPT"
+		IR_COMMENT[MonitoringLocationIdentifier %in% accept_mlids] = "Manually accepted"
+		IR_FLAG[MonitoringLocationIdentifier %in% accept_mlids] = "ACCEPT"
+		IR_MLID[MonitoringLocationIdentifier %in% accept_mlids] = MonitoringLocationIdentifier[MonitoringLocationIdentifier %in% accept_mlids]
+		IR_MLNAME[MonitoringLocationIdentifier %in% accept_mlids] = MonitoringLocationName[MonitoringLocationIdentifier %in% accept_mlids]
+		IR_Lat[MonitoringLocationIdentifier %in% accept_mlids] = LatitudeMeasure[MonitoringLocationIdentifier %in% accept_mlids]
+		IR_Long[MonitoringLocationIdentifier %in% accept_mlids] = LongitudeMeasure[MonitoringLocationIdentifier %in% accept_mlids]	
+	})
+
+	### Re-build reactive_objects$map_sites
+	reason_mlids=unique(reasons[reasons$Reason %in% input$review_reasons,'MonitoringLocationIdentifier'])
+	reactive_objects$map_sites=reactive_objects$sites[reactive_objects$sites$IR_FLAG %in% input$site_types & reactive_objects$sites$MonitoringLocationIdentifier %in% reason_mlids,]
+	
+	### Clear table selection & update map highlights (via reactive_objects$selected_sites)
+	reactive_objects$selected_sites=reactive_objects$selected_sites[!reactive_objects$selected_sites %in% reactive_objects$table_selected_mlids]
+	
+	### Clear modal
+	removeModal()
+
+})
+
+
+
+
 #observeEvent(input$reject, {})
 #observeEvent(input$merge, {})
 #observeEvent(input$comment, {})
-
-
-
-
-
-
-
 
 
 
