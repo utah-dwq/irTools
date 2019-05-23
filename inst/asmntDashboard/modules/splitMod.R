a5369c30266b4f608a27ff86395791f356a5108e
Jacob Vanderlaan <jvander@utah.gov>
2019-05-23 16:57:20 -0600

    splitMod

---
+++ inst/asmntDashboard/modules/splitMod.R
@@ -0,0 +1,31 @@
+
+# AU split module
+
+splitModUI <-function(id){
+	ns <- NS(id)
+	tagList(	
+		shinycssloaders::withSpinner(editModUI(ns("auSplit"), height='600px', width='600px'),size=2, color="#0080b7")
+	)
+}
+
+splitMod <- function(input, output, session, selected_aus, sel_sites, au_asmnt_poly, site_asmnt, na_sites, rejected_sites){
+	reactive_objects=reactiveValues()
+	
+	au_asmnt_poly=subset(au_asmnt_poly, ASSESS_ID %in% selected_aus)
+	view=sf::st_bbox(au_asmnt_poly)
+	site_asmnt=subset(site_asmnt, IR_MLID %in% sel_sites)
+	sel_aus_map=asmntMap(au_asmnt_poly, site_asmnt, na_sites, rejected_sites) %>%
+		fitBounds(paste(view[1]),paste(view[2]),paste(view[3]),paste(view[4])) %>%
+		showGroup('Assessed sites')
+
+	return(callModule(editMod, "auSplit", sel_aus_map, targetLayerId='split_shapes'))
+
+	#observe({
+	#	req(splits()$finished)
+	#	print(splits()$finished)
+	#})
+	}
+
+
+
+
