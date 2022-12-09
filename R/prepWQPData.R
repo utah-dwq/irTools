5c8a0983e29ae2a01a683f92954044f5441294e4
Elise Hinman <ehinman@utah.gov>
2022-12-08 19:45:28 -0700

    prepWQPData update
    Fixed character check export option so viewer can see what special characters were converted to NA or converted to a number if ", |< |> " was part of the string.

--- R/prepWQPData.R
+++ R/prepWQPData.R
@@ -1,17 +1,15 @@
 #' Prep WQP Data
 #'
-#' This function reads dataframes downloaded from EPA's WQP, removes duplicates, and converts result values with non-numeric characters to NA (with the exception of those with <|>|,).
+#' This function reads dataframes downloaded from EPA's WQP, removes duplicates, and converts result values and detection limit values with non-numeric characters to NA (with the exception of those with <|>|,).
 #' Converts all non-result value columns in nr, activity, detquantlim, and sites objects to character, including dates.
 #' Replaces hard coding in bookdown and readWQPFiles function.
 #'
-#' @param nr A narrow result dataframe from EPA's WQP
-#' @param activity An activity result dataframe from EPA's WQP
-#' @param detquantlim A detection quantitation limits dataframe from EPA's WQP
-#' @param sites A sites dataframe from EPA's WQP
-#' @return A list consisting of nr, activity, detquantlim, sites, filtered sites, and merged_results (nr combined with activity) dataframes with special characters converted to NA for the ResultMeasureValue column. Saves an rdata file named "irdata_wqp_raw.Rdata" containing the six data WQP objects.
+#' @param irdata A list of WQP dataframes containing sites, narrowresult, activity, and detquantlim objects.
+#' @param char_check Logical. If TRUE, the function produces a .csv showing all the special characters changed or converted to NA.
+#' @return A list consisting of nr, activity, detquantlim, sites, filtered sites, and merged_results (nr combined with activity) dataframes with special characters converted to NA for the ResultMeasureValue column. Saves an rdata file named "irdata_prepped.Rdata" containing the six data WQP objects.
 
 #' @export
-prepWQPData <- function(irdata, cols, check=TRUE){
+prepWQPData <- function(irdata, char_check=TRUE){
   
   # Get unique records - WQP had some duplication issues in 04/2022
   print("Reading in dataframes, removing duplicate values, converting all columns to character...")
@@ -50,18 +48,18 @@ prepWQPData <- function(irdata, cols, check=TRUE){
   
   merged_results = merge(nr, activity, all.x = TRUE)
   
+  ### Checks - if true, produces a csv showing which unique values were converted to NA
+  if(char_check==TRUE){
+    nr_check = subset(nr, !is.na(nr$ResultMeasureValue_raw))
+    nr_check = unique(nr_check[,c("ResultIdentifier","ResultMeasureValue_raw","ResultMeasureValue","CharacteristicName")])
+    nr_check = subset(nr_check, is.na(nr_check$ResultMeasureValue)|grepl(",|<|>", nr_check$ResultMeasureValue_raw))
+    nr_check$ResultMeasureValue[is.na(nr_check$ResultMeasureValue)]="NA"
+    write.csv(nr_check, "special_char_check.csv", row.names = FALSE)
+    print("Special character check completed. 'special_char_check.csv' saved to working directory.")
+  }
   # built irdata list of dataframes from WQP
   irdata = list(nr=nr,activity=activity,detquantlim=detquantlim,sites=sites,sites_filtered=sites_filtered, merged_results=merged_results)
   save(irdata, file = "irdata_prepped.Rdata")
+  
   return(irdata)
-  ### Checks - if true, produces a csv showing which unique values were converted to NA
-  if(check==TRUE){
-    nr_check = subset(nr, !is.na(nr$ResultMeasureValue_raw))
-    nr_check = unique(nr_check[,c("ResultIdentifier","ResultMeasureValue_raw","ResultMeasureValue","CharacteristicName")])
-    nr_check$ResultMeasureValue[is.na(nr_check$ResultMeasureValue)] = "NA"
-    nr_check$same = ifelse(nr_check$ResultMeasureValue==nr_check$ResultMeasureValue_raw,1,0)
-    nr_check_notsame = subset(nr_check, nr_check$same==0)
-    write.csv(nr_check_notsame, "special_char_check.csv", row.names = FALSE)
-    print("Special character check completed. 'special_char_check.csv' saved to working directory.")
-  }
 }
